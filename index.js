ec2292e015754db0cdf76978eafacae66c9fcffe
RetrozDev <ryanbarbozapro@gmail.com>
2024-02-06 15:11:17 +0100

    Create express base

---
+++ index.js
@@ -0,0 +1,27 @@
+  const movies = require('./movies.json') 
+  const express = require('express');
+  const app = express();
+  
+  app.get('/', (req, res) => {
+    res.send('Welcome to my favourite movie list');
+  });
+  
+  app.get('/api/movies', (req, res) => {
+    res.status(200).json(movies);
+  });
+  
+  app.get('/api/movies/:id', (req, res) => {
+    const movieId = parseInt(req.params.id);
+    const foundMovie = movies.find(movie => movie.id === movieId);
+  
+    if (foundMovie) {
+      res.status(200).json(foundMovie);
+    } else {
+      res.status(404).send('Not Found');
+    }
+  });
+  
+  const port = 6969;
+  app.listen(port, () => {
+    console.log(`Le serveur est ouvert \n http://localhost:${port}/`);
+  });
